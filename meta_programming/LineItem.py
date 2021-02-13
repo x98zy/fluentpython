1a3a5f23edd535aa3b6fbc527baecbb3ca8a2080
xuzhiyi1998 <1580987871@qq.com>
2021-02-13 08:58:37 +0800

    改进特性

--- meta_programming/LineItem.py
+++ meta_programming/LineItem.py
@@ -1,6 +1,6 @@
 """实现可读可写的特性"""
 
-class LineItem():
+class LineItem1():
     def __init__(self,weight,price):
         self.weight=weight
         self.price=price
@@ -18,6 +18,25 @@ class LineItem():
         else:
             raise ValueError("weight值必须大于0")
 
+
+class LineItem():
+    """改进版LineItem"""
+    def __init__(self,weight,price):
+        self.weight=weight
+        self.price=price
+
+    def get_weight(self):
+        return self._weight
+
+    def set_weight(self,value):
+        if value>0:
+            self._weight=value
+        else:
+            raise  ValueError("weight must be positive")
+
+    weight=property(fget=get_weight,fset=set_weight)
+
+
 if __name__=="__main__":
-    item=LineItem(1,6.5)
+    item=LineItem(-2,5)
     print(item.weight)
